6b01930deab9006a1f0fe631e1826c19f71a185b
DevelopJKong <ybaram40@gmail.com>
2022-11-06 12:17:19 +0900

    컴포넌트 - 컴포넌트 배치 완료

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { HelloComponent } from './hello/hello.component';
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+import { TimeDisplayComponent } from './section/time-display/time-display.component';
+import { ButtonsComponent } from './section/buttons/buttons.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HelloComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, SectionComponent, TimeDisplayComponent, ButtonsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/section/time-display/time-display.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TimeDisplayComponent } from './time-display.component';
+
+describe('TimeDisplayComponent', () => {
+  let component: TimeDisplayComponent;
+  let fixture: ComponentFixture<TimeDisplayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TimeDisplayComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TimeDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
