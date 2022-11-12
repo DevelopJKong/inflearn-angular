767ef4c4f4662b7fb48b5a6c1cd9d4f1a9e37ff3
DevelopJKong <ybaram40@gmail.com>
2022-11-12 16:12:19 +0900

    컴포넌트 - 데이터 바인딩 1 완료

--- src/app/section/buttons/buttons.component.ts
+++ src/app/section/buttons/buttons.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
-  styleUrls: ['./buttons.component.scss']
+  styleUrls: ['./buttons.component.scss'],
 })
 export class ButtonsComponent implements OnInit {
+  count = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  start($event: MouseEvent) {
+    this.count++;
   }
-
 }
--- src/app/section/time-display/time-display.component.ts
+++ src/app/section/time-display/time-display.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-time-display',
   templateUrl: './time-display.component.html',
-  styleUrls: ['./time-display.component.scss']
+  styleUrls: ['./time-display.component.scss'],
 })
 export class TimeDisplayComponent implements OnInit {
+  test: number = 1;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setInterval(() => {
+      this.test++;
+    },1000);
   }
 
+  ngOnInit(): void {}
 }
